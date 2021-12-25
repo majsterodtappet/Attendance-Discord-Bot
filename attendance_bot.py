a8a6f62e2b80a6cd6b26eb1d772808aa18374327
majsterodtappet <89748341+majsterodtappet@users.noreply.github.com>
2021-12-26 00:18:28 +0100

    Update attendance_bot.py
    - removed pytz package
    - added command prefix
    - updated UI

--- attendance_bot.py
+++ attendance_bot.py
@@ -1,40 +1,34 @@
 import discord
+from discord.ext import commands
 from discord.utils import get
-from pytz import timezone
-import os
+from datetime import datetime
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name='Mark your Attendance!'))
     print("bot is ONLINE")
 
-@client.event
-async def on_message(message):
-  
-   if message.author == client.user:
-     return
+@client.command()
+async def attendance(message):
 
-   if message.content == '!attendance':
-
-        await message.delete()
+        await message.message.delete()
 
         embed = discord.Embed(
-            description = f'‎\nNew attendance list. **Mark your presence!**\n‎',
+            description = f'New attendance list. **Mark your presence!**\n‎',
             color = 0xffffff)
 
-        embed.set_author(name=message.created_at.astimezone(timezone('Europe/Warsaw')).strftime('ATTENDANCE %d.%m.%Y'))
-        embed.set_thumbnail(url='https://media.discordapp.net/attachments/706796095719866469/921016124500508682/attendance.png')
+        embed.set_author(name=datetime.now().strftime('MEETING %d.%m.%Y'))
+        embed.set_thumbnail(url='https://media.discordapp.net/attachments/706796095719866469/924434268552314900/attendanceM.png')
 
         embed.add_field(name='✅  PRESENT', value=f'React below to mark your presence!')
-        embed.add_field(name='⛔  CLOSE THE LIST', value=f'Only authorized person closes the list.')
+        embed.add_field(name='⛔  CLOSE THE LIST', value=f'Only authorized user closes the list.')
 
         msg = await message.channel.send(embed=embed)
 
         await msg.add_reaction("✅")
         await msg.add_reaction("⛔")
-
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -59,7 +53,7 @@ async def on_raw_reaction_add(payload):
             color=0x62b546)
 
         embed.set_author(name= priorEmbed.author.name)
-        embed.set_thumbnail(url= priorEmbed.thumbnail.url)
+        embed.set_thumbnail(url='https://media.discordapp.net/attachments/706796095719866469/921016124500508682/attendance.png')
 
         embed.add_field(name='✅  ATTENDANCE LIST', value='\n'.join([f'{x.mention}' for x in closed]))
         embed.add_field(name='📊  ATTENDANCE COUNT', value=f'**{len(closed)}** participated in the meeting.')
